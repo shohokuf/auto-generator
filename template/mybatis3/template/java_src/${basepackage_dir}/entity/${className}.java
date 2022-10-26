1b34b983e22db40d85b57fbe5111804889550a2d
sunchuanfu <shohokuf@163.com>
2022-10-26 14:33:22 +0800

    1

--- template/mybatis3/template/java_src/${basepackage_dir}/entity/${className}.java
+++ template/mybatis3/template/java_src/${basepackage_dir}/entity/${className}.java
@@ -1,74 +1,74 @@
 <#include "/macro.include"/>
-        <#assign className = table.className>
-        <#assign classNameLower = className?uncap_first>
-        package ${basepackage}.entity;
+<#assign className = table.className>
+<#assign classNameLower = className?uncap_first>
+package ${basepackage}.entity;
 
 import ${basepackage}.entity.BaseEntity;
 
 public class ${className} extends BaseEntity {
 
-private static final long serialVersionUID = 1L;
-<#list table.columns as column>
+    private static final long serialVersionUID = 1L;
+    <#list table.columns as column>
 
-/**
- * @备注:${column.remarks}
- * @字段:${column.sqlName} ${column.sqlTypeName}(${column.size})
- * ${column.remarks}
- * ${column.sqlName} ${column.sqlTypeName}(${column.size})
- */
-private ${column.javaType} ${column.columnNameLower};
-<#if column.isDateTimeColumn>
-</#if>
-</#list>
-<@generateConstructor className/>
-<@generateJavaColumns/>
-        }
+    /**
+     * @备注:${column.remarks}
+     * @字段:${column.sqlName} ${column.sqlTypeName}(${column.size})
+     * ${column.remarks}
+     * ${column.sqlName} ${column.sqlTypeName}(${column.size})
+     */
+    private ${column.javaType} ${column.columnNameLower};
+    <#if column.isDateTimeColumn>
+    </#if>
+    </#list>
+    <@generateConstructor className/>
+    <@generateJavaColumns/>
+}
 <#macro generateJavaColumns>
-<#list table.columns as column>
-public void set${column.columnName}(${column.javaType} ${column.columnNameLower}) {
+    <#list table.columns as column>
+    public void set${column.columnName}(${column.javaType} ${column.columnNameLower}) {
         this.${column.columnNameLower} = ${column.columnNameLower};
-        }
+    }
 
-public ${column.javaType} get${column.columnName}() {
+    public ${column.javaType} get${column.columnName}() {
         return this.${column.columnNameLower};
-        }
-<#if column.isDateTimeColumn>
+    }
+    <#if column.isDateTimeColumn>
 
-</#if>
-</#list>
+    </#if>
+    </#list>
 </#macro>
 <#macro generateJavaOneToMany>
-<#list table.exportedKeys.associatedTables?values as foreignKey>
-<#assign fkSqlTable = foreignKey.sqlTable>
-<#assign fkTable    = fkSqlTable.className>
-<#assign fkPojoClass = fkSqlTable.className>
-<#assign fkPojoClassVar = fkPojoClass?uncap_first>
+    <#list table.exportedKeys.associatedTables?values as foreignKey>
+    <#assign fkSqlTable = foreignKey.sqlTable>
+    <#assign fkTable    = fkSqlTable.className>
+    <#assign fkPojoClass = fkSqlTable.className>
+    <#assign fkPojoClassVar = fkPojoClass?uncap_first>
 
-private Set ${fkPojoClassVar}s = new HashSet(0);
-public void set${fkPojoClass}s(Set<${fkPojoClass}> ${fkPojoClassVar}){
+    private Set ${fkPojoClassVar}s = new HashSet(0);
+    public void set${fkPojoClass}s(Set<${fkPojoClass}> ${fkPojoClassVar}){
         this.${fkPojoClassVar}s = ${fkPojoClassVar};
-        }
+    }
 
-public Set<${fkPojoClass}> get${fkPojoClass}s() {
+    public Set<${fkPojoClass}> get${fkPojoClass}s() {
         return ${fkPojoClassVar}s;
-        }
-</#list>
+    }
+    </#list>
 </#macro>
 <#macro generateJavaManyToOne>
-<#list table.importedKeys.associatedTables?values as foreignKey>
-<#assign fkSqlTable = foreignKey.sqlTable>
-<#assign fkTable    = fkSqlTable.className>
-<#assign fkPojoClass = fkSqlTable.className>
-<#assign fkPojoClassVar = fkPojoClass?uncap_first>
+    <#list table.importedKeys.associatedTables?values as foreignKey>
+    <#assign fkSqlTable = foreignKey.sqlTable>
+    <#assign fkTable    = fkSqlTable.className>
+    <#assign fkPojoClass = fkSqlTable.className>
+    <#assign fkPojoClassVar = fkPojoClass?uncap_first>
 
-private ${fkPojoClass} ${fkPojoClassVar};
+    private ${fkPojoClass} ${fkPojoClassVar};
 
-public void set${fkPojoClass}(${fkPojoClass} ${fkPojoClassVar}){
+    public void set${fkPojoClass}(${fkPojoClass} ${fkPojoClassVar}){
         this.${fkPojoClassVar} = ${fkPojoClassVar};
-        }
+    }
 
-public ${fkPojoClass} get${fkPojoClass}() {
+    public ${fkPojoClass} get${fkPojoClass}() {
         return ${fkPojoClassVar};
-        }
-</#list>
+    }
+    </#list>
 </#macro>
